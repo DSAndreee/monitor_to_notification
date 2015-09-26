114fe447006cc3f756e25eb60f41cff5e8111de3
DSAndreee <andre.da-silva@etudiant.univ-reims.fr>
2015-09-26 21:34:37 +0200

    Update monitor.sh

--- monitor.sh
+++ monitor.sh
@@ -1,7 +1,7 @@
 if gem list -i terminal-notifier; then
     clear
 else
-    echo "Installation de terminal-notifier..."
+    echo "Installing terminal-notifier packages..."
     echo ""
     echo ""
     sudo gem install terminal-notifier
@@ -9,15 +9,15 @@ else
     echo ""
     echo ""
     fi
-echo "Merci d'entrer une IP ou un Hostname à monitorer :"
+echo "Please enter below IP or hostname that you want to monitor :"
 read YOUR_ISP
 echo ""
-echo "Nombre de drop d'affilés avant notification (recommandation = 2) :"
+echo "Number of drop before notification (recommandation = 2) :"
 read DROP_NB
 clear
-terminal-notifier -message "Le monitoring est en place vers $YOUR_ISP (Sensibilité : $DROP_NB drop)." -title "Bravo !" -sound default
+terminal-notifier -message "Monitoring for $YOUR_ISP is now operational (Sensitivity : $DROP_NB drop)." -title "Here we are !" -sound default
 while :
 do
-  ping  -t $DROP_NB -c $DROP_NB $YOUR_ISP || terminal-notifier -message "Oups. Il semblerait que $YOUR_ISP soit down." -title "Alerte Monitoring !" -open $YOUR_ISP -sound default
+  ping  -t $DROP_NB -c $DROP_NB $YOUR_ISP || terminal-notifier -message "Oops. It seems that $YOUR_ISP is down." -title "Monitoring alert !" -open $YOUR_ISP -sound default
   sleep 1
 done
